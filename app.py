926eb1074c79a5631151f5b906669557fa6d0dde
dimama24-meet <dmarzouqa@gmail.com>
2024-08-21 19:30:33 +0300

    lab

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
-print('dima')
-print('hello world')
+def create_youtube_video(title,description):
+	youtube_video={"title":title ,"description": description,"likes": 0,"dislikes":0,"comments": {}}
+	return youtube_video
+def likes(youtube_video):
+	if 'likes' in youtube_video:
+		youtube_video['likes'] += 1
+		return
+
+def dislikes(youtube_video):
+	if 'dislikes' in youtube_video:
+		youtube_video['dislikes'] += 1
+		return
+def add_comments (youtubevideo,username,comment_text):
+	youtubevideo['comments'][username]=comment_text
+print (create_youtube_video("slime in 3 min","best way to do slime"))
