2608230b9381d07dbd5b92357765c846644725f3
dimama24-meet <dmarzouqa@gmail.com>
2024-08-22 00:28:57 +0300

    py lab

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ def create_youtube_video(title,description):
 def likes(youtube_video):
 	if 'likes' in youtube_video:
 		youtube_video['likes'] += 1
+		
 		return
 
 def dislikes(youtube_video):
 	if 'dislikes' in youtube_video:
 		youtube_video['dislikes'] += 1
+		
 		return
 def add_comments (youtubevideo,username,comment_text):
 	youtubevideo['comments'][username]=comment_text
+	return
 print (create_youtube_video("slime in 3 min","best way to do slime"))
+
+def video(title,description,youtube_video,username,comment_text):
+	 video=_add_commesne(video,'thank you for the helpful video')
+	 
+	 youtube_video['likes'] +=495
