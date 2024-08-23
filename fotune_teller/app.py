2367081c5eb0e9f39986b276864abb7c2aea220c
dimama24-meet <dmarzouqa@gmail.com>
2024-08-23 10:32:07 +0300

    lab

--- fotune_teller/app.py
+++ fotune_teller/app.py
@@ -6,22 +6,46 @@ static_folder='static')
 
 @app.route('/home',methods=["GET","POST"])
 def home():
-	if request.method == 'GET':
-		return render_template('home.html')
+	if request.method == 'POST':
+		birth_month = request.form['birth_month']
+		return redirect(url_for('fortune', birth_month=birth_month))
 	else:
-		return redirect(url_for('fortune',
-			Birth_Month= Birth_Month,))
-		name = request.form['Birth_Month']
+		return render_template('home.html')
 
-	return render_template("home.html")
-
-@app.route('/fortune')
-def fortune():
+@app.route('/fortune/<string:birth_month>')
+def fortune(birth_month):
 	my_fortune=["Read people mindes","Be rich for the rest of your life","Die in a car crash",
-	"get back with time"]
-	fortune=random.choice(my_fortune)
-	return render_template("fortune.html", fortune=fortune)
+	"get back with time","you will marry your enemy","Someone you love will die",
+	"A surprise will brighten your day!","Peace will surround you for the rest of your life","someone you know will kill you"]
+	#len(birth_month)
+	#print(my_fortune[len(birth_month)-1])
+	#fortune=random.choice(my_fortune)
+	if 3<= len(birth_month) <=9:
+		return render_template("fortune.html", fortune=my_fortune[len(birth_month)-1])
+	else:
+		return render_template("home.html")
 
 
+
+'''
+@app.route('/fortune/<birth_month>')
+def fortune(birth_month):
+    # Example fortune based on birth month
+    fortunes = {
+    'January': 'You will have a productive month!',
+    'February': 'Love is in the air for you!',
+    'March': 'Adventure awaits you this month!',
+    'April': 'Expect positive changes in your life!',
+    'May': 'Your hard work will pay off soon!',
+    'June': 'New opportunities are on the horizon!',
+    'July': 'You will enjoy peace and relaxation!',
+    'August': 'Success is coming your way!',
+    'September': 'A surprise will brighten your day!',
+    'October': 'You will make meaningful connections!',
+    'November': 'Abundance is in your future!',
+    'December': 'Joy and happiness will surround you!'}
+	fortune_message = fortunes.get(birth_month, "Sorry, I don't have a fortune for that month.")
+	return f"Your fortune: {fortune_message}"
+'''
 if __name__ == '__main__':
     app.run(debug = True)
